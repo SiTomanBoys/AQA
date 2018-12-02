64bf6e715ce455e545e1dfaf3c3de2650aa54102
Asus <Asus@NekoAsus>
2018-12-02 20:57:56 -0300

    mensaje :D

---
+++ ApiRest/RestAQA/src/main/java/cl/AQA/ApiRest/RestAQA/RestAqaApplication.java
@@ -0,0 +1,12 @@
+package cl.AQA.ApiRest.RestAQA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAqaApplication.class, args);
+	}
+}
