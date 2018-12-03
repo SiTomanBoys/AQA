bb3d4afb1e0d2c837f10b6514cdfd98c05d5c235
Anibal <Anibal@WIN-O9KVA35S3B3>
2018-12-02 21:17:50 -0300

    upd

--- ApiRest/RestAQA/src/main/java/cl/AQA/ApiRest/RestAqaApplication.java
+++ ApiRest/RestAQA/src/main/java/cl/AQA/ApiRest/RestAqaApplication.java
@@ -2,11 +2,30 @@ package cl.AQA.ApiRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jmx.export.MBeanExporter;
 
 @SpringBootApplication
-public class RestAqaApplication {
+public class RestAqaApplication extends SpringBootServletInitializer 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(RestAqaApplication.class, args);
+	}
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestAqaApplication.class);
+    }
+ 
+	@Bean
+	public MBeanExporter exporter()
+	{
+	    final MBeanExporter exporter = new MBeanExporter();
+	    exporter.setAutodetect(true);
+	    exporter.setExcludedBeans("dataSource");
+	    return exporter;
 	}
 }
